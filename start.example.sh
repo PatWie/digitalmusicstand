728ef1ae60217d7800959022cb56cbe63042a20d
PatWie <mail@patwie.com>
2018-02-10 21:39:15 +0100

    fix start_example

--- start.example.sh
+++ start.example.sh
@@ -1,4 +1,4 @@
 SHEET_DIR="path/to/sheets"
 PORT=8889
 
-sudo docker run -i -p ${PORT}:8888 -v ${SHEET_DIR}:"/app/data" -t digitalmusicstand
+sudo docker run -i -p ${PORT}:8888 -v ${SHEET_DIR}:"/app/data" -t patwie/digitalmusicstand
